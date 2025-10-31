6a47beb74fb3dc8d8086e9828e962fea94bd6449
Vanshika garg <vnamratagarg35@gmail.com>
2025-10-31 12:53:01 +0530

    Time: 0 ms (100%), Space: 11.7 MB (11.88%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        int count[3]={0};
+        for(int num: nums){
+            count[num]++;
+        }
+        int index=0;
+        for(int i=0;i<3;i++){
+            while(count[i]-->0){
+                nums[index++]=i;
+            }
+        }
     }
 };
