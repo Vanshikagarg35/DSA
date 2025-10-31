89b2b48e90043c6b0a06fab949a83cb3faf46d83
Vanshika garg <vnamratagarg35@gmail.com>
2025-10-31 14:27:16 +0530

    Time: 719 ms (16.18%), Space: 50.6 MB (45.8%) - LeetHub

---
+++ 1027-sum-of-even-numbers-after-queries/1027-sum-of-even-numbers-after-queries.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> sumEvenAfterQueries(vector<int>& nums,
+                                    vector<vector<int>>& queries) {
+        vector<int> output;
+
+        for (int i = 0; i < queries.size(); i++) {
+
+            nums[queries[i][1]] = nums[queries[i][1]] + queries[i][0];
+
+            int sum = 0;
+            for (int n : nums) {
+                if (n % 2 == 0) {
+                    sum = sum + n;
+                }
+            }
+
+            output.push_back(sum);
+        }
+
+        return output;
+    }
+};
