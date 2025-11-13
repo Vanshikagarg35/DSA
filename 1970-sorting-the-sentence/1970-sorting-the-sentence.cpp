6fd11ed594a8b50ab0131fffa8952ad57161b45e
Vanshika garg <vnamratagarg35@gmail.com>
2025-11-13 13:16:35 +0530

    Time: 0 ms (100%), Space: 8.5 MB (38.49%) - LeetHub

---
+++ 1970-sorting-the-sentence/1970-sorting-the-sentence.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string sortSentence(string s) {
+        string temp ="";
+        map<char , string> mp;
+        for(int i = 0; i<s.size(); i++){
+            char ch = s[i];
+            if(isdigit(ch)){
+                mp[ch]=temp;
+                temp="";
+            }
+            else if(isalpha(ch)){
+                temp+=ch;
+            }
+            else{
+                continue;
+            }
+
+
+        }
+        string result;
+        for (auto it = mp.begin(); it != mp.end(); ++it) {
+            result += it->second + " ";
+        }
+        while (!result.empty() && result.back() == ' ') result.pop_back();
+        return result;
+    }
+};
