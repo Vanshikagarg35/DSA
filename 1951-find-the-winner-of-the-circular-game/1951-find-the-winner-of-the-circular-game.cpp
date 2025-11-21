935e0631f0b3272cce19d51c38f5657f8c67ea1c
Vanshika garg <vnamratagarg35@gmail.com>
2025-11-21 14:22:13 +0530

    Time: 0 ms (100%), Space: 7.9 MB (63.74%) - LeetHub

--- 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.cpp
@@ -1,31 +1,13 @@
 class Solution {
 public:
-    int winner(vector<bool>& person, int n, int index, int personLeft, int k) {
-        if(personLeft == 1) {
-            for(int i = 0; i < n; i++) {
-                if(person[i] == 0) { 
-                    return i;
-                }
-            }
-        }
-        int kill = (k - 1) % personLeft;
-        while(kill--) {
-            index = (index + 1) % n;
-            while(person[index] == 1) {
-                index = (index + 1) % n;
-            }
+    int winner(int n, int k) {
+
+        if (n == 1) {
+            return 0;
         }
 
-        person[index] = 1;
-        while(person[index] == 1) {
-            index = (index + 1) % n;
-        }
+        return (winner(n - 1, k) + k) % n;
+    }
 
-        return winner(person, n, index, personLeft - 1, k);
-    }
-    
-    int findTheWinner(int n, int k) {
-        vector<bool> person(n, 0);
-        return winner(person, n, 0, n, k) + 1; 
-    }
+    int findTheWinner(int n, int k) { return winner(n, k) + 1; }
 };
