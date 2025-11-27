57a3ea136984dc15c6ccdf65ef2d42d67383f397
Vanshika garg <vnamratagarg35@gmail.com>
2025-11-27 22:03:25 +0530

    Time: 0 ms (100%), Space: 8 MB (84.13%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -1,29 +1,15 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        int count =0;
-        string result = "";
-        while(count < s.size()){
-            result = rotateclockwise(s);
-            count++;
-            if(result == goal){
-                return true;
-            }
-            else{
-                result = rotateclockwise(result);
-            }
-        }
-        return false;
-    }
-    string rotateclockwise(string &s){
-        char c = s[s.size()-1];
-        int index = s.size()-2;
-        while(index>=0){
-            s[index+1] = s[index];
-            index--;
+        if (s.size() != goal.size()) return false;
 
-        }
-        s[0]=c;
-        return s;
+        string doubled = s + s;  
+
+        int idx = doubled.find(goal);
+
+        if (idx < doubled.size()) 
+            return true;
+
+        return false;
     }
 };
