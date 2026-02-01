2b29e697f092d7fe6c14c6c98e705211982495aa
Vanshika garg <vnamratagarg35@gmail.com>
2026-02-01 20:33:40 +0530

    Time: 0 ms (100%), Space: 9.4 MB (10.8%) - LeetHub

--- 0859-buddy-strings/0859-buddy-strings.cpp
+++ 0859-buddy-strings/0859-buddy-strings.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
     bool buddyStrings(string s, string goal) {
-        if (s.size() != goal.size()) return false;
-
-        if (s == goal) {
-            unordered_set<char> st;
-            for (char c : s) {
-                st.insert(c);
-            }
-            return st.size() < s.size();  
-        }
-
-        vector<int> t;
-        for (int i = 0; i < s.size(); i++) {
-            if (s[i] != goal[i]) {
-                t.push_back(i);
+        if(s.length() != goal.length()) return false;
+        
+        unordered_set<char> sett(begin(s), end(s));
+        if(s == goal && s.length() != sett.size()) return true;
+        
+        vector<int> st;
+        for(int i = 0; i < s.length(); i++) {
+            if(s[i] != goal[i]) {
+                st.push_back(i);
             }
         }
-
-        if (t.size() != 2) return false;
-
-        int i = t[0], j = t[1];
-        return s[i] == goal[j] && s[j] == goal[i];
+        
+        if(st.size() == 2) {
+            int i = st[0];
+            int j = st[1];
+    
+            if(s[i] == goal[j] && s[j] == goal[i]) return true;
+            
+        }
+        return false;
     }
 };
