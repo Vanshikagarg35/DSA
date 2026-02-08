e8fdee0dce1bca39293b4f7fc4a101966914d7b8
Vanshika garg <vnamratagarg35@gmail.com>
2026-02-08 13:59:52 +0530

    Time: 39 ms (57.09%), Space: 106 MB (10.89%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
@@ -1,50 +1,23 @@
 class Solution {
 public:
-    
-    int findBest(int target_idx, int k, vector<int>& nums, vector<long>& prefixSum) {
-        int target = nums[target_idx];
-        
-        int i = 0;
-        int j = target_idx;
-        int result = target_idx;
-        
-        while(i <= j) {
-            int mid = i + (j-i)/2;
-            
-            long count      = (target_idx - mid + 1);
-            long windowSum  = (count * target);
-            long currSum    = prefixSum[target_idx] - prefixSum[mid] + nums[mid];
-            
-            int ops         = windowSum - currSum;
-            
-            if(ops > k) {
-                i = mid+1;
-            } else {
-                result = mid;
-                j = mid-1;
-            }
-        }
-        
-        return target_idx-result+1;
-    }
-    
     int maxFrequency(vector<int>& nums, int k) {
-        int n = nums.size();
+        sort(nums.begin(), nums.end());
         
-        sort(begin(nums), end(nums));
-        vector<long> prefixSum(n);
-        prefixSum[0] = nums[0];
+        int left = 0;
+        long long sum = 0;
+        int maxFreq = 0;
         
-        for(int i = 1; i < n; i++) {
-            prefixSum[i] = prefixSum[i-1] + nums[i];
+        for(int right = 0; right < nums.size(); right++) {
+            sum += nums[right];
+
+            while((long long)nums[right] * (right - left + 1) - sum > k) {
+                sum -= nums[left];
+                left++;
+            }
+            
+            maxFreq = max(maxFreq, right - left + 1);
         }
         
-        int result = 0;
-        
-        for(int i = 0; i<n; i++) {
-            result = max(result, findBest(i, k, nums, prefixSum));
-        }
-        
-        return result;
+        return maxFreq;
     }
 };
