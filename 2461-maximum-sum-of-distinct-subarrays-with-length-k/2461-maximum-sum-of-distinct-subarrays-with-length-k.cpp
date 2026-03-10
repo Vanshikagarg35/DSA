79244badf4a749c37d368a95504d9f7e42d3bb71
Vanshika garg <vnamratagarg35@gmail.com>
2026-03-10 12:15:10 +0530

    Time: 103 ms (59.78%), Space: 98.3 MB (45.55%) - LeetHub

---
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    long long maximumSubarraySum(vector<int>& nums, int k) {
+        int i = 0;
+        int j = 0;
+        long long sum = 0;    
+        long long maxSum = 0; 
+        int count = 0;
+        unordered_map<int,int> freq;
+
+        while(j < nums.size()){
+            sum += nums[j];
+            freq[nums[j]]++;
+            count++;
+
+            while(freq[nums[j]] > 1 || count>k) { 
+                sum -= nums[i];
+                freq[nums[i]]--;
+                count--;
+                i++;
+            }
+
+            if(count == k){
+                maxSum = max(sum, maxSum);
+            }
+            
+            j++;
+        }
+        return maxSum;
+    }
+};
