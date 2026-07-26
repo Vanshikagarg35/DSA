7e9543d1974c243820f482b72de7dd914592c1d9
Vanshika garg <vnamratagarg35@gmail.com>
2026-07-26 17:00:21 +0530

    Time: 180 ms (59.3%), Space: 120.3 MB (58.56%) - LeetHub

--- 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
@@ -2,30 +2,19 @@ class Solution {
 public:
     int n;
     long long t[1000001][2];
-    long long solve(int idx, vector<int>& nums, bool iseven) {
-        
-        if(idx >= n) {
-            return 0;
-        }
-        if(t[idx][iseven] != -1)
-            return t[idx][iseven];
-        
-        long long skip = solve(idx+1, nums, iseven);
-        
-        long long val = nums[idx];
-        if(iseven == false) {
-            val = -val;
-        }
-        
-        long long take = solve(idx+1, nums, !iseven) + val;
-        
-        return t[idx][iseven] = max(skip, take);
-        
+    long long solve(int idx , vector<int>& nums , bool iseven){
+        if(idx>=nums.size()) return 0;
+        if(t[idx][iseven]!=-1) return t[idx][iseven];
+        int val = nums[idx];
+        if(iseven == false) val = -val;
+        long long take = solve(idx+1 , nums , !iseven)+val;
+        long long skip = solve(idx+1 , nums, iseven);
+        return t[idx][iseven] = max(take , skip);
     }
-    
     long long maxAlternatingSum(vector<int>& nums) {
-        n = nums.size();
-        memset(t, -1, sizeof(t));
-        return solve(0, nums, true);
+        memset(t , -1, sizeof(t));
+        return solve(0 , nums, true);
+
+        
     }
 };
