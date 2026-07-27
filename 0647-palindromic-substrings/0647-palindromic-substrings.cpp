52ff3a00138541bfedb3d5584d96211296642471
Vanshika garg <vnamratagarg35@gmail.com>
2026-07-27 13:48:13 +0530

    Time: 31 ms (31.04%), Space: 13 MB (37.09%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,31 +1,22 @@
 class Solution {
 public:
-
-    bool check(string &s, int i, int j) {
-        if(i >= j) {
-            return true;
-        }
-
-        if(s[i] == s[j]) {
-            return check(s, i+1, j-1);
-        }
-
-        return false;
-
+    int t[1001][1001];
+    bool solve(int i, int j, string &s){
+        if(i>=j) return true;
+        if(t[i][j]!=-1)return t[i][j];
+        if(s[i]==s[j]) return t[i][j] = solve(i+1 , j-1, s);
+        return t[i][j] = false;
     }
-
     int countSubstrings(string s) {
         int n = s.length();
-
         int count = 0;
-        for(int i = 0; i < n; i++) {
-            for(int j = i; j < n; j++) { //check all possible substrings
-                if(check(s, i, j)) {
-                    count++;
-                }
+        memset(t , -1 , sizeof(t));
+        for(int i=0; i<n; i++){
+            for(int j=i; j<n; j++){
+                if(solve(i,j,s)) count++;
             }
-        }
 
+        }
         return count;
     }
 };
