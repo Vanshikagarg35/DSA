6d79b217beba670057ba81a23a1b32ed12f989b1
Vanshika garg <vnamratagarg35@gmail.com>
2026-07-31 21:19:00 +0530

    Time: 50 ms (34.79%), Space: 10.2 MB (86.78%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
@@ -1,35 +1,29 @@
 class Solution {
 public:
-    int t[301][11];
-    int solve(vector<int>& jobDifficulty, int n, int idx, int d) {
-        if (d == 1) {
-            return *max_element(begin(jobDifficulty) + idx, end(jobDifficulty));
+int t[301][11];
+int solve(int d , int idx, vector<int>& jobDifficulty){
+    int n = jobDifficulty.size();
+    if(d==1){
+        int maxD = INT_MIN;
+        for(int i=idx; i<n; i++){
+            maxD = max(maxD , jobDifficulty[i]);
         }
-        
-        if (t[idx][d] != -1)
-            return t[idx][d];
-    
-        int Max = INT_MIN;
-        int result = INT_MAX;
-        
-
-        for (int i = idx; i <= n - d; i++) {
-            Max = max(Max, jobDifficulty[i]);
-            result = min(result, Max + solve(jobDifficulty, n, i + 1, d - 1));
-        }
-        
-        return t[idx][d] = result;
+        return maxD;
     }
-    
+    if(t[idx][d]!=-1) return t[idx][d];
+    int maxD = INT_MIN;
+    int result = INT_MAX;
+    for(int i=idx; i<=n-d; i++){
+        maxD = max(maxD , jobDifficulty[i]);
+        result = min(result,maxD + solve(d-1 , i+1, jobDifficulty));
+    }
+    return t[idx][d] = result;
+}
     int minDifficulty(vector<int>& jobDifficulty, int d) {
-        int n = jobDifficulty.size();
-        
-        if (n < d)
-            return -1;
+        if(jobDifficulty.size() < d) return -1;
+        memset(t , -1 , sizeof(t));
+        return solve(d , 0 , jobDifficulty);
 
-        memset(t, -1, sizeof(t));
         
-        return solve(jobDifficulty, n, 0, d);
     }
 };
-
